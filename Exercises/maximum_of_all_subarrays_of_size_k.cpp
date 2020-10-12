157c167042efd45b22d8c6ffc080309fd40574f2
antoninolorefice <72207980+27151725@users.noreply.github.com>
2020-10-12 15:52:05 +0200

    Update maximum_of_all_subarrays_of_size_k.cpp

--- Exercises/maximum_of_all_subarrays_of_size_k.cpp
+++ Exercises/maximum_of_all_subarrays_of_size_k.cpp
@@ -1,40 +1,49 @@
-// #include <iostream>
-// #include <string>
-// using namespace std;
+#include <iostream>
+#include <deque>
 
-// // problem: https://practice.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k/0
+using namespace std;
 
-// void print_max_all_subarray(int values[], int size, int k)
-// {
-//     int maximum = 0;
-//     int i;
-//     for (i = 0; i < k; i++){
-//         maximum = max(maximum, values[i]); // the maximum of the first three elements
-//     }
-//     std::cout << maximum << " ";
-//     for (i = k; i < size; i++){
-//         maximum = max(maximum, values[i]);
-//         std::cout << maximum << " "; // the maximum between the maximum and the i-th element
-//     }
-//     std::cout << std::endl;
-// }
+// problem: https://practice.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k/0
 
-// int main()
-// {
-//     int num_test_cases = 0;
-//     std::cin >> num_test_cases;
-//     for (int i = 0; i < num_test_cases; i++) {
-//         int size = 0; // size of i th test case
-//         std::cin >> size;
-//         int k = 0; // the length of the subarray
-//         std::cin >> k;
-//         int values[size]; // array of elements of i th test case
-//         int value = 0;
-//         for (int j = 0; j < size; j++) {
-//             std::cin >> value;
-//             values[j] = value;
-//         }
-//         print_max_all_subarray(values, size, k);
-//     }
-//     return 0;
-// }
+void print_max_all_subarray(int values[], int size, int k)
+{
+    std::deque<int> indexes;
+    int maxs_length = size - k + 1;
+    int maxs[maxs_length];
+    for (int i = 0; i < size; i++) {
+        while (!indexes.empty() && indexes.front() <= i - k) {
+            indexes.pop_front(); // remove index of element that not is in the subarray
+        }
+        while (!indexes.empty() && values[i] >= values[indexes.back()]) {
+            indexes.pop_back(); // remove index of element that is minor than the current
+        }
+        indexes.push_back(i);
+        if (i >= k - 1) {
+            maxs[i - k + 1] = values[indexes.front()];
+        }
+    }
+    for (int j = 0; j < maxs_length; j++) {
+        std::cout << maxs[j] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    int num_test_cases = 0;
+    std::cin >> num_test_cases;
+    for (int i = 0; i < num_test_cases; i++) {
+        int size = 0; // size of i th test case
+        std::cin >> size;
+        int k = 0; // the length of the subarray
+        std::cin >> k;
+        int values[size]; // array of elements of i th test case
+        int value = 0;
+        for (int j = 0; j < size; j++) {
+            std::cin >> value;
+            values[j] = value;
+        }
+        print_max_all_subarray(values, size, k);
+    }
+    return 0;
+}
